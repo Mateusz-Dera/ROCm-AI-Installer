5c357211b8631d4c4859aab8937c4ef88bf96760
Mateusz-Dera <OtahoPL@gmail.com>
2024-07-23 23:49:27 +0200

    fixed flash-attn Transformers && ExLlama2

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ It should also work on 7900XT cards.
 For other cards, change HSA_OVERRIDE_GFX_VERSION and GFX at the beginning of the script (Not tested).
 
 ## Info
-[![Version](https://img.shields.io/badge/4.5.2-version-orange.svg)](https://github.com/Mateusz-Dera/ROCm-AI-Installer/blob/main/README.md)
+[![Version](https://img.shields.io/badge/4.6-version-orange.svg)](https://github.com/Mateusz-Dera/ROCm-AI-Installer/blob/main/README.md)
 
 > [!Note]
-> Ubuntu 24.04 is recommended. Version 4.5.2 is not tested on older versions.
+> Ubuntu 24.04 is recommended. Version 4.6 is not tested on older versions.
 
 Part of the installation script is based on this guide: https://github.com/nktice/AMD-AI/blob/main/ROCm6.0.md
 
@@ -26,7 +26,7 @@ Part of the installation script is based on this guide: https://github.com/nktic
 |Name|Enviroment|Links|Additional information|
 |:---|:---|:---|:---|
 |KoboldCPP|Python 3.12 venv|https://github.com/YellowRoseCx/koboldcpp-rocm||
-|Text generation web UI|Python 3.12 venv|https://github.com/oobabooga/text-generation-webui<br/> https://github.com/arlo-phoenix/bitsandbytes-rocm-5.6<br/> https://github.com/ROCmSoftwarePlatform/flash-attention<br/> https://github.com/turboderp/exllamav2</br> https://github.com/abetlen/llama-cpp-python|1. Tested: ExLlamav2, llama.ccp, Transformers (without Flash Attention)<br> 2. Requrements for Superbooga are installed, but the extension is not enabled by default |
+|Text generation web UI|Python 3.11 venv|https://github.com/oobabooga/text-generation-webui<br/> https://github.com/arlo-phoenix/bitsandbytes-rocm-5.6<br/> https://github.com/ROCmSoftwarePlatform/flash-attention<br/> https://github.com/turboderp/exllamav2</br> https://github.com/abetlen/llama-cpp-python|1. Tested: ExLlamav2, llama.ccp, Transformers<br> 2. Requrements for Superbooga are installed, but the extension is not enabled by default |
 |SillyTavern (1.12.2)|Node|https://github.com/SillyTavern/SillyTavern|1. Extras project is discontinued and removed from the script.<br> https://github.com/SillyTavern/SillyTavern-Extras|
 
 ###  <span style="color: orange;">Image generation</font>
--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ export HSA_OVERRIDE_GFX_VERSION=11.0.0
 GFX=gfx1100
 
 # Version
-version="4.5.2"
+version="4.6"
 
 # Default installation path
 default_installation_path="$HOME/AI"
@@ -391,8 +391,8 @@ EOF
 
 # Text generation web UI
 install_text_generation_web_ui() {
-    if ! command -v python3.12 &> /dev/null; then
-        echo "Install Python 3.12 first"
+    if ! command -v python3.11 &> /dev/null; then
+        echo "Install Python 3.11 first"
         exit 1
     fi
 
@@ -401,44 +401,55 @@ install_text_generation_web_ui() {
     rm -rf text-generation-webui
     git clone https://github.com/oobabooga/text-generation-webui.git
     cd text-generation-webui
-    git checkout 2f71515cb0624f9686b488935f482d423a60b274
-    python3.12 -m venv .venv --prompt TextGen
+    git checkout 6b4d76212060d618534e4ab976534eee75f5466d
+    python3.11 -m venv .venv --prompt TextGen
     source .venv/bin/activate
 
+    pip install --upgrade pip
+
+    pip install cmake ninja wheel setuptools packaging --extra-index-url https://download.pytorch.org/whl/nightly/rocm6.1
+
     pip install --pre cmake colorama filelock lit numpy Pillow Jinja2 \
-	mpmath fsspec MarkupSafe certifi filelock networkx \
-	sympy packaging requests \
-    --extra-index-url https://download.pytorch.org/whl/rocm6.0
+        mpmath fsspec MarkupSafe certifi filelock networkx \
+        sympy packaging requests \
+            --index-url https://download.pytorch.org/whl/nightly/rocm6.1
 
-    pip install --pre torch torchvision torchaudio pytorch-triton pytorch-triton-rocm \
-    --extra-index-url https://download.pytorch.org/whl/rocm6.0
+    pip install --pre -U torch==2.5.0.dev20240704+rocm6.1 torchvision==0.20.0.dev20240704+rocm6.1 \
+    torchaudio pytorch-triton pytorch-triton-rocm \
+    --index-url https://download.pytorch.org/whl/nightly/rocm6.1
 
-    pip install -U wheel setuptools
+    pip install https://download.pytorch.org/whl/cpu/torchtext-0.18.0%2Bcpu-cp311-cp311-linux_x86_64.whl#sha256=c760e672265cd6f3e4a7c8d4a78afe9e9617deacda926a743479ee0418d4207d --extra-index-url https://download.pytorch.org/whl/nightly/rocm6.1
+    
+    git clone https://github.com/ROCm/bitsandbytes.git
+    cd bitsandbytes
+    git checkout 43d39760e5490239330631fd4e61f9d00cfc8479
+    pip install . --extra-index-url https://download.pytorch.org/whl/nightly/rocm6.1
 
-    pip install git+https://github.com/ROCm/bitsandbytes.git@43d39760e5490239330631fd4e61f9d00cfc8479
-    pip install git+https://github.com/ROCmSoftwarePlatform/flash-attention.git@2554f490101742ccdc56620a938f847f61754be6 --no-build-isolation
+    cd $installation_path/text-generation-webui
 
-    pip install -r requirements_amd.txt --extra-index-url https://download.pytorch.org/whl/rocm6.0
+    git clone https://github.com/ROCmSoftwarePlatform/flash-attention.git
+    cd flash-attention
+    git checkout 2554f490101742ccdc56620a938f847f61754be6
+    pip install -e . --no-build-isolation --extra-index-url https://download.pytorch.org/whl/nightly/rocm6.1
 
-    pip uninstall -y llama-cpp-python
-    pip uninstall -y llama_cpp_python_cuda
+    cd $installation_path/text-generation-webui
 
-    # CMAKE_ARGS="-DLLAMA_HIPBLAS=on -DCMAKE_C_COMPILER=/opt/rocm/llvm/bin/clang -DCMAKE_CXX_COMPILER=/opt/rocm/llvm/bin/clang++ -DCMAKE_PREFIX_PATH=/opt/rocm" FORCE_CMAKE=1 pip install llama-cpp-python==0.2.74
-    git clone  --recurse-submodules https://github.com/abetlen/llama-cpp-python.git repositories/llama-cpp-python 
-    cd repositories/llama-cpp-python
-    git checkout 7e20e346bd49cc8f0031eb053fe879a38c777b6f
-    pip install --force-reinstall torch torchtext torchvision torchaudio torchrec --extra-index-url https://download.pytorch.org/whl/rocm6.0
-    pip install .  -C cmake.args="-DAMDGPU_TARGETS=$GFX -DLLAMA_HIPBLAS=ON -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release"
-
-    cd $installation_path/text-generation-webui/
+    pip install -r requirements_amd.txt --extra-index-url https://download.pytorch.org/whl/nightly/rocm6.1
 
     git clone https://github.com/turboderp/exllamav2 repositories/exllamav2
     cd repositories/exllamav2
     git checkout 6a8172cfce919a0e3c3c31015cf8deddab34c851
-    pip install . --extra-index-url https://download.pytorch.org/whl/rocm6.0
+    pip install . --extra-index-url https://download.pytorch.org/whl/nightly/rocm6.1
 
-    cd $installation_path/text-generation-webui/extensions/superbooga
-    pip install -r ./requirements.txt --extra-index-url https://download.pytorch.org/whl/rocm6.0
+    pip uninstall -y llama_cpp_python
+    pip uninstall -y llama_cpp_python_cuda
+
+    cd $installation_path/text-generation-webui
+
+    git clone --recurse-submodules https://github.com/abetlen/llama-cpp-python.git repositories/llama-cpp-python 
+    cd repositories/llama-cpp-python
+    git checkout 816d4912d9d2971198d2300a840ce4c100152502
+    CC='/opt/rocm/llvm/bin/clang' CXX='/opt/rocm/llvm/bin/clang++' CFLAGS='-fPIC' CXXFLAGS='-fPIC' CMAKE_PREFIX_PATH='/opt/rocm' ROCM_PATH="/opt/rocm" HIP_PATH="/opt/rocm" CMAKE_ARGS="-GNinja -DLLAMA_HIPBLAS=ON -DLLAMA_AVX2=on " pip install --no-cache-dir .
 
     cd $installation_path/text-generation-webui
 
@@ -447,7 +458,7 @@ install_text_generation_web_ui() {
 export HSA_OVERRIDE_GFX_VERSION=11.0.0
 export CUDA_VISIBLE_DEVICES=0
 source $installation_path/text-generation-webui/.venv/bin/activate
-python server.py --api --listen --loader=exllamav2 --extensions sd_api_pictures send_pictures gallery
+TORCH_BLAS_PREFER_HIPBLASLT=0 python server.py --api --listen --loader=exllamav2 --extensions sd_api_pictures send_pictures gallery
 EOF
     chmod u+x run.sh
 }
