b4f41e162422021f6b2aad5f3abe44ba921c8c4c
Mateusz-Dera <OtahoPL@gmail.com>
2025-05-31 00:55:16 +0200

    rocm

--- install.sh
+++ install.sh
@@ -109,24 +109,17 @@ repo(){
     sudo apt install -y wget
 
     # AMDGPU
-    sudo apt-add-repository -y -s -s
-    sudo apt install -y "linux-headers-$(uname -r)" \
-	"linux-modules-extra-$(uname -r)"
     sudo mkdir --parents --mode=0755 /etc/apt/keyrings
     wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
-    gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
-    echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.4.1/ubuntu noble main' \
-    | sudo tee /etc/apt/sources.list.d/amdgpu.list
-    sudo apt update -y
-    # sudo apt install -y amdgpu-dkms
+        gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
 
-    # ROCm
+
+    # Register ROCm packages
     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.4.1 noble main" \
-    | sudo tee --append /etc/apt/sources.list.d/rocm.list
+        | sudo tee --append /etc/apt/sources.list.d/rocm.list
     echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
-    | sudo tee /etc/apt/preferences.d/rocm-pin-600
-    sudo apt update -y
-    sudo apt install -y rocm-dev rocm-libs rocm-hip-sdk rocm-libs
+        | sudo tee /etc/apt/preferences.d/rocm-pin-600
+    sudo apt update
 }
 
 profile(){
@@ -161,6 +154,7 @@ EOF
 
     profile
 
+    sudo apt install -y rocm
     sudo apt install -y git git-lfs
     sudo apt install -y libstdc++-12-dev
     sudo apt install -y libtcmalloc-minimal4
