cb9e402f217c1d83a1e1d1e8330ba1b5f5db0e0d
Mateusz-Dera <OtahoPL@gmail.com>
2025-10-30 17:21:29 +0100

    amdgpu

--- install.sh
+++ install.sh
@@ -125,7 +125,7 @@ EOF
 
     sudo apt update -y
     sudo apt install -y "linux-headers-$(uname -r)"
-    sudo apt install -y amd-gpu rocminfo rocm-cmake hipblas hipcc hipify-clang rocm-hip-runtime rocm-hip-runtime-dev 
+    sudo apt install -y amdgpu rocminfo rocm-cmake hipblas hipcc hipify-clang rocm-hip-runtime rocm-hip-runtime-dev 
 }
 
 #? Install ZLUDA
