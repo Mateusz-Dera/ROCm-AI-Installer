4764ec9469ba910529bbcc29b915892bf8bd8f7d
Mateusz-Dera <OtahoPL@gmail.com>
2025-12-15 19:14:06 +0100

    Fixes

--- interfaces.sh
+++ interfaces.sh
@@ -74,7 +74,7 @@ if ! podman ps --format \"{{.Names}}\" | grep -q \"^rocm\\\$\"; then
     echo \"Container rocm is not running. Starting...\"
     podman start rocm
 fi
-podman exec -t rocm bash -c \"cd /AI/$FOLDER $VENV $COMMAND\"
+exec podman exec -it rocm bash -c \"cd /AI/$FOLDER $VENV $COMMAND\"
 RUNEOF
 chmod +x /AI/$FOLDER/run.sh"
 }
